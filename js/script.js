9d0886b0661975200ef23d2aaf268cbc67346c0c
Ntnick-22 <nyeinthunaing322@gmail.com>
2025-04-14 10:44:27 +0100

    2nd edited

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const collegeAddress = {
+  name: "National College of Ireland",
+  street: "Mayor Street Lower",
+  area: "IFSC",
+  city: "Dublin",
+  postcode: "D01 K6W2",
+  country: "Ireland",
+};
